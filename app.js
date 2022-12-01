0de38f5b15f490989efc04c95d45786becee31b8
Ulises Pérez <perez.ulisesernesto@gmail.com>
2022-12-01 15:21:19 +0100

    java

---
+++ app.js
@@ -0,0 +1,34 @@
+const nombre = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const warning = document.getElementById("warning");
+const bttn = document.getElementById("bttn");
+const w = document.getElementById("warn")
+const wemail = document.getElementById("wemail")
+
+bttn.addEventListener("click",(e)=>{
+  e.preventDefault()
+  let warnings=""
+  let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+  let p = ""
+  let w= ""
+if(number.value.length != 9){
+    warnings="Not valid"
+    warning.innerHTML = warnings;
+}
+
+    if (nombre.value == "ironhack"){
+        warn="You cannot be Ironhack, because I am Ironhack."
+        w.innerHTML = warn;
+    }
+if (!valid.test(email.value))
+{
+    p = "Not valid"
+    wemail.innerHTML = p;
+
+}
+})
+
+
+
+    
